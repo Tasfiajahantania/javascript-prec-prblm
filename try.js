023715fee9f1e2ec1e8619a16bd5da76bbfd8bf6
Tasfiajahan <tania.develpers@gmail.com>
2022-08-09 00:45:03 +0600

    first commit

---
+++ try.js
@@ -0,0 +1,41 @@
+///////////////////////////-----------------prec-1------------------/////////////////////////
+let giveMoney = 1000;
+let applePrice = 300;
+let orangePrice = 400;
+let totalCost = applePrice + orangePrice;
+let monyback = giveMoney - totalCost;
+console.log(monyback);
+
+
+///////////////////////////------------------prec-2--------------------/////////////////////////
+
+let biology = 80;
+let chemistry = 90;
+let physics = 78;
+let bangla = 99;
+let totalMark = biology + chemistry + physics + bangla;
+let avg = totalMark / 4;
+console.log(avg);
+
+//////////////////////////------------------------------------------/////////////////////////////
+let firstLine = 'I am going to be an';
+let lustLine = 'awesome webdeveloper';
+let fullLine = firstLine + ' ' + lustLine;
+console.log(fullLine);
+
+
+/////////////////////////-------------------------------------------//////////////////////////
+
+const dailyTask = ["Unlock module at 8", "Do Practice with videoes", "Take notes while watching video", "practice module by own after video", "If dont understand anything"];
+for (let i = 0; i < dailyTask.length; i++){
+    const element = dailyTask[i];
+    console.log(element);
+    if (element == 'if do not anything') {
+        console.log('join our support session');
+    }
+}
+
+////////////////////////////////--------------------------------//////////////////////////////
+for (let i = 1; i <= 10; i++){
+    console.log('ajke amr mon vlo nei', i);
+}
